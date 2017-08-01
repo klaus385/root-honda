7d7565a3490828f0bb5b2fad91f4cdb9f0f0092d
Stephen Klaus <sklaus@newtekone.com>
2017-08-01 12:49:12 -0700

    Used Shellcheck to clean up the script.

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ if [ "$1" = "" ]; then
 	exit 0
 fi
 
-uname=`uname`
+uname=$(uname)
 echo "OS type: $uname"
 
 echo "Getting signature of $1..."
 if [ "$uname" = "Darwin" ]; then
-	sig=`java -jar bin/GetAndroidSig.jar "$1" | grep "To char" | sed -E 's/^.{9}//'`
+	sig=$(java -jar bin/GetAndroidSig.jar "$1" | grep "To char" | sed -E 's/^.{9}//')
 else
-	sig=`java -jar bin/GetAndroidSig.jar "$1" | grep "To char" | sed -r 's/^.{9}//'`
+	sig=$(java -jar bin/GetAndroidSig.jar "$1" | grep "To char" | sed -r 's/^.{9}//')
 fi
 echo "Signature: $sig"
 
 echo "Getting package information..."
 if [ "$uname" = "Darwin" ]; then
-	package=`aapt dump permissions "$1" | head -1 | sed -E 's/^.{9}//'`
+	package=$(aapt dump permissions "$1" | head -1 | sed -E 's/^.{9}//')
 else
-	package=`aapt dump permissions "$1" | head -1 | sed -r 's/^.{9}//'`
+	package=$(aapt dump permissions "$1" | head -1 | sed -r 's/^.{9}//')
 fi
 echo "Package name: $package"
 
@@ -73,33 +73,33 @@ else
 	exit 1
 fi
 
-wlcheck=`ls -al whitelist-1.0.xml | awk '{print $5}'`
-if [ $wlcheck -gt 20000 ]; then
+wlcheck=$(ls -al whitelist-1.0.xml | awk '{print $5}')
+if [ "$wlcheck" -gt 20000 ]; then
 	echo "Original whitelist-1.0.xml size seems okay"
 else
 	echo "Error: Original whitelist-1.0.xml size DOES NOT seem okay!"
 	exit 1
 fi
 
-packagecheck=`grep $package whitelist-1.0-new.xml`
-if [ ! -z "$package" ]; then
+packagecheck=$(grep "$package" whitelist-1.0-new.xml)
+if [ ! -z "$packagecheck" ]; then
 	echo "Package name is present in new whitelist"
 else
 	echo "Error: Package name is NOT present in new whitelist!"
 	exit 1
 fi
 
-ts=`date '+%Y-%m-%d--%H-%M-%S'`
+ts=$(date '+%Y-%m-%d--%H-%M-%S')
 echo "Backing up whitelist to /data/local/tmp/whitelist-1.0-$ts.xml"
 adb shell "cp /data/data/whitelist-1.0.xml /data/local/tmp/whitelist-1.0-$ts.xml"
 echo "Making an extra backup of whitelist on your PC..."
-adb pull /data/local/tmp/whitelist-1.0-$ts.xml .
+adb pull /data/local/tmp/whitelist-1.0-"$ts".xml .
 
 echo "Uploading whitelist to tmp..."
 adb push whitelist-1.0-new.xml /data/local/tmp/whitelist-1.0-new.xml
 
 adb shell "su -c 'cp /data/local/tmp/whitelist-1.0-new.xml /data/data/whitelist-1.0.xml'"
 adb shell "su -c 'chown system:system /data/data/whitelist-1.0.xml'"
-adb install -r $1
+adb install -r "$1"
 #adb push $1 /data/local/tmp/$1
 #adb shell "su -c 'pm install -r /data/local/tmp/$1'"
